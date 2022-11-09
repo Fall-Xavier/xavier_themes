0195508bc3aab11a55dd7b554504f605b1bd127d
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2022-11-09 16:14:15 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-  name = "xavier",
-  packages = ["xavier"],
-  version = "0.7",
+  name = "xavier themes",
+  packages = ["xavier_themes"],
+  version = "0.0.4",
   license ="MIT",
-  description = "This library is used to make it easier for developers or users to get random user agents from https://useragents.io.",
+  description = "Library Python Beautiful Text And Panel In Terminal",
   long_description = long_description,
   long_description_content_type = "text/markdown", 
   author = "Fall Xavier",
-  author_email = "fallxavier@gmail.com",
+  author_email = "falahfajrul802@gmail.com",
   url = "https://github.com/Fall-Xavier/xavier",
   download_url = "https://github.com/Fall-Xavier/xavier/archive/xavier.tar.gz", 
   keywords = ["rich", "module rich", "xavier", "module xavier"], 
@@ -24,6 +24,6 @@ setup(
     "Intended Audience :: Developers",
     "Topic :: Software Development :: Build Tools",
     "License :: OSI Approved :: MIT License",
-    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
   ],
 )
